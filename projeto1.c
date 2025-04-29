eb2ee177d22539d7a559950df60135d5435136c3
Antonio Carlos <antonio.carlos.48slz@gmail.com>
2025-04-29 18:56:10 -0300

    primeiro_commit

--- projeto1.c
+++ projeto1.c
@@ -4,6 +4,7 @@
 #include "hardware/sync.h"
 #include "hardware/gpio.h"
 #include "pico/multicore.h"
+#include "pico/time.h"
 
 #define JOY_X_PIN 26
 #define JOY_Y_PIN 27
@@ -12,7 +13,8 @@
 #define LED_AZUL 12
 #define BUZZER 21
 
-volatile int prioridade = 0;
+volatile int prioridade = 0; 
+
 
 void core1_main() {
     while (true) {
@@ -23,11 +25,11 @@ void core1_main() {
         gpio_put(LED_VERMELHO, 0);
         gpio_put(BUZZER, 0);
 
-        if (atual == 1){
+        if (atual == 1) {
             gpio_put(LED_AZUL, 1);
-        } else if(atual == 2) {
+        } else if (atual == 2) {
             gpio_put(LED_VERDE, 1);
-        } else if(atual == 3) {
+        } else if (atual == 3) {
             gpio_put(LED_VERMELHO, 1);
             gpio_put(BUZZER, 1);
         }
@@ -35,37 +37,46 @@ void core1_main() {
         sleep_ms(100);
     }
 }
+
+
+int64_t joystick_timer_callback(alarm_id_t id, void *user_data) {
+    adc_select_input(0); uint16_t x = adc_read();
+    adc_select_input(1); uint16_t y = adc_read();
+
+    if (x > 3500 || y > 3500)
+        prioridade = 3;
+    else if (x > 2500 || y > 2500)
+        prioridade = 2;
+    else if (x > 1500 || y > 1500)
+        prioridade = 1;
+    else
+        prioridade = 0;
+
+    return 100; 
+}
+
 int main() {
     stdio_init_all();
 
-    gpio_init(LED_VERDE);
-    gpio_init(LED_AZUL);
-    gpio_init(LED_VERMELHO);
-    gpio_init(BUZZER);
+    
+    gpio_init(LED_VERDE);    gpio_set_dir(LED_VERDE, GPIO_OUT);
+    gpio_init(LED_AZUL);     gpio_set_dir(LED_AZUL, GPIO_OUT);
+    gpio_init(LED_VERMELHO); gpio_set_dir(LED_VERMELHO, GPIO_OUT);
+    gpio_init(BUZZER);       gpio_set_dir(BUZZER, GPIO_OUT);
 
-    gpio_set_dir(LED_VERDE, GPIO_OUT);
-    gpio_set_dir(LED_AZUL, GPIO_OUT);
-    gpio_set_dir(LED_VERMELHO, GPIO_OUT);
-    gpio_set_dir(BUZZER, GPIO_OUT);
-
+   
     adc_init();
-    adc_gpio_init(JOY_X_PIN);
-    adc_gpio_init(JOY_Y_PIN);
+    adc_gpio_init(JOY_X_PIN); 
+    adc_gpio_init(JOY_Y_PIN); 
+
 
     multicore_launch_core1(core1_main);
 
-    while (true)
-    {
-       adc_select_input(0); 
-       uint16_t x = adc_read();
-       adc_select_input(1); 
-       uint16_t y = adc_read();
 
-       if (x > 3500 || y > 3500) prioridade = 3;
-        else if (x > 2500 || y > 2500) prioridade = 2;
-        else if (x > 1500 || y > 1500) prioridade = 1;
-        else prioridade = 0;
+    add_alarm_in_ms(100, joystick_timer_callback, NULL, true);
 
-        sleep_ms(100);
+ 
+    while (true) {
+        tight_loop_contents(); 
     }
 }
